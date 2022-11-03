b80de44117dae950c2e57315939f7eb734c2fc37
Gabriel Damay <gabriel.damay@telecom-paris.fr>
2022-11-03 17:32:09 +0100

    Add documentation

--- Models/PointSet/Point.h
+++ Models/PointSet/Point.h
@@ -1,34 +1,118 @@
-// Define a point of the dataset
 #ifndef POINT_H_INCLUDED
 #define POINT_H_INCLUDED
 
 #include <array>
 #include <string>
 
+/**
+ * Point of data from the dataset.
+ *
+ * A single point of data from the dataset, including the features data from
+ * the point, the decision value and the dimension, which should be equal in
+ * each point of the dataset.
+ *
+ * @note This is meant for points that are constants after being constructed.
+ */
 class Point {
 	private:
+		/// Dimension of the features array.
 		size_t dimension;
+
+		/**
+		 * Features data of the point.
+		 *
+		 * @note This is owned by the object.
+		 */
 		float* features;
+
+		/// Decision value of the point
 		bool value;
 	
 	public:
+		/**
+		 * Main constructor of Point.
+		 *
+		 * Construct a point by providing each of its attributes.
+		 *
+		 * @param dimension Dimension of the features array.
+		 * @param features Features of the point.
+		 * 	Memory is copy, hence this does not take ownership of the parameter
+		 * @param value The decision value of the point
+		 */
 		Point(size_t dimension, const float* features, const bool value);
-		// dimension is the dimension of the features, should be number of parameters + 1
+
+		/**
+		 * Interprete a file line as a point.
+		 *
+		 * Deprecated : does not manage binary/categorised features
+		 */
+		[[deprecated("Use point_from_file() instead")]]
 		Point(std::string input, size_t dimension, char delimiter, unsigned int label_position, float label_true_value);
+
+		/**
+		 * Point class assignment operator.
+		 *
+		 * Copy the data of the source object.
+		 *
+		 * @param source The object to copy.
+		 */
 		Point& operator=(const Point& source);
-		
-		// For  multiset.
-		// This is smaller if the first non-equal feature is smaller
-		// Or all features are equals and value is smaller 
+
+		/**
+		 * Compare Points.
+		 *
+		 * Compare Points by defining the smaller as the one of which the first
+		 * feature that is not equal to the corresponding feature in the other
+		 * point is smaller. If all features are equals, the smaller Point is
+		 * the one of value false.
+		 *
+		 * @param other Point to compare to current point.
+		 * @returns true if this is smaller than other.
+		 * @note This is meant for making multisets of Point enable.
+		 */
 		bool operator<(const Point& other) const;
+
+		/**
+		 * Test Points equality.
+		 *
+		 * @param other Point to compare to current point.
+		 * @returns true iff all features and value are equals.
+		 */
 		bool operator==(const Point& other) const;
+
+		/**
+		 * Copy constructor of Point.
+		 *
+		 * Create a Point by copying all features and value of source
+		 *
+		 * @param source Point from which data will be copied
+		 */
 		Point(const Point& source);
+
+		/**
+		 * Destructor of Point.
+		 *
+		 * Free memory of features.
+		 */
 		~Point();
-		
+
+		/// Dimension of features array
 		size_t get_dimension();
+
+		/// Decision value of the point
 		bool get_value();
+
+		/**
+		 * Feature of the Point at given position
+		 *
+		 * @param position Position of the feature to get in the features array
+		 */
 		float get_feature(std::size_t position);
+
+		/// Array of features of the Point.
 		const float* get_features();
+
+		/// Bracket operator equivalent to get_feature(size_t)
 		float operator[](std::size_t position);
 		
 };
--- Models/PointSet/PointSet.h
+++ Models/PointSet/PointSet.h
@@ -1,4 +1,3 @@
-// Define a set of points of the dataset
 #ifndef POINTSET_H_INCLUDED
 #define POINTSET_H_INCLUDED
 
@@ -7,35 +6,142 @@
 #include <vector>
 #include "Point.h"
 
-enum class FeatureType {BINARY, CLASSIFIED, REAL};
+/// Possible types of features.
+enum class FeatureType {
+	BINARY, //< Binary feature, can be 0 or 1.
+	CLASSIFIED, //< Categorized feature, discrete and without logical order.
+	REAL //< Real feature, (almost) continuous and logically ordered.
+};
 
+/**
+ * A subset of the dataset.\ Contains a collection of Points.
+ *
+ * This class handles most of the calculations that have to be made on the
+ * points, including gini gain and splitting along best feature.
+ */
 class PointSet {
 	private:
+		/**
+		 * Multiset of the points in the PointSet.
+		 *
+		 * @note Each point is not owned by the PointSet.
+		 * @note Each point dimension shpuld be equal to this->dimension.
+		 */
 		std::multiset<Point*> points;
+
+		/**
+		 * Types of each feature of the dataset points.
+		 *
+		 * Vector of size "dimension" containing, for each feature, its type.
+		 */
 		std::vector<FeatureType> features_types;
+
+		/**
+		 * Relevance of each feature for gain calculation.
+		 *
+		 * Vector of size "dimension" containing true for each feature except
+		 * the binary/categorial ones that have already been used for splitting
+		 * the PointSet of an upper Vertex, and hence will be equal for all
+		 * Points.
+		 *
+		 * @note For categorial features, this will be false only if the
+		 *	vertex is under the "true" leg of the split vertex, since
+		 * 	on the "false" leg, diversity could still exist between values
+		 * 	other than the split one.
+		 */
 		std::vector<bool> is_feature_relevent;
+
+		/// Dimension of the features array of the points.
 		size_t dimension;
+
+		/// Count number of Points which have value equal "true".
 		unsigned int positive_counter;
+
+		/// Proportion of Points that have value equal "true".
 		float positive_proportion;
+
+		/**
+		 * Keep track of calling of get_positive_proportion().
+		 *
+		 * positive_proportion should be recalculated at each update of the
+		 * PointSet. Hence, this boolean is true if positive_proportion should
+		 * be recalculated before use.
+		 */
 		bool is_positive_proportion_calculated;
+
+		/// Gini of this PointSet
 		float gini;
+
+		/**
+		 * Keep track of calling of get_gini().
+		 *
+		 * Data related to gini should be recalculated at each update of the
+		 * PointSet. Hence, this boolean is true if those data should be
+		 * recalculated before use.
+		 */
 		bool is_gini_calculated;
-		//used for keeping track of number of points <= threashold (0 for first version) for each parameter
+
+		/**
+		 * Number of points on left leg of the best split.
+		 *
+		 * If the split is on a real feature, this is the number of points for
+		 * whose this feature is under threashold. If the split is on a
+		 * binary/categorial feature, this is the number of points for whose
+		 * this feature is different from threashold.
+		 */
 		int best_under_counter;
-		//used for keeping track of number of positive points <= threashold (0 for first version) for each parameter
+		/**
+		 * Number of positive points on left leg of the best split.
+		 *
+		 * If the split is on a real feature, this is the number of points with
+		 * value equals true for whose this feature is under threashold. If the
+		 * split is on a binary/categorial feature, this is the number of
+		 * points with value equals true for whose this feature is different
+		 * from threashold.
+		 */
 		int best_under_positive_counter;
-		//used for keeping track of number of points > threashold (0 for first version) for each parameter
+		/**
+		 * Number of points on right leg of the best split.
+		 *
+		 * If the split is on a real feature, this is the number of points for
+		 * whose this feature is over threashold. If the split is on a
+		 * binary/categorial feature, this is the number of points for whose
+		 * this feature equals threashold.
+		 */
 		int best_over_counter;
-		//used for keeping track of number of positive points > threashold (0 for first version) for each parameter
+		/**
+		 * Number of positive points on right leg of the best split.
+		 *
+		 * If the split is on a real feature, this is the number of points with
+		 * value equals true for whose this feature is over threashold. If the
+		 * split is on a binary/categorial feature, this is the number of
+		 * points with value equals true for whose this feature equals
+		 * threashold.
+		 */
 		int best_over_positive_counter;
 
-		// Proxy lineraly dependant on real gain
+		/**
+		 * Proxy of gain for best feature/threashold.
+		 *
+		 * Proxy linearly dependant on the gini gain of splitting along
+		 * feature/threshold that maximise this gain.
+		 */
 		double best_gain;
 
+		/// Feature along which splitting maximise gini gain.
 		size_t best_parameter;
+		/// Threashold in best feature along which splitting maximise gini gain
 		float best_threshold;
+		/**
+		 * Keep track of calling of calculate_best_gain().
+		 *
+		 * Data related to best gain should be recalculated at each update of
+		 * the PointSet. Hence, this boolean is true if those data should be
+		 * recalculated before use.
+		 */
 		bool is_gain_calculated;
 
+		/// Update data related to best gini gain.
 		void calculate_best_gain();
 	
 	public:
